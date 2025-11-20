26adbcd9ec2ba96b91431cc553afe0f4c1840799
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-20 15:03:11 +1100

    feat: add tailwind to components

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -15,19 +15,20 @@ const Card: React.FC<Props> = ({
   onPortfolioCreate,
 }: Props): JSX.Element => {
   return (
-    <div className="card">
-      <img alt="companyLogo"></img>
-      <div className="details">
-        <h2>
-          {searchResult.name}({searchResult.symbol})
-        </h2>
-        <p>${searchResult.currency}</p>
-      </div>
-      <p className="info">{searchResult.exchangeFullName}</p>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">{searchResult.exchangeFullName}</p>
       <AddPortfolio
         onPortfolioCreate={onPortfolioCreate}
         symbol={searchResult.symbol}
-      ></AddPortfolio>
+      />
     </div>
   );
 };
