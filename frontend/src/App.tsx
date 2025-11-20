731c1fe368bc4a0e10f5f9db6dacab2a15c2a124
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-20 11:42:09 +1100

    feat: add btn now creates and updates portfolio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,11 @@ import Search from "./Components/Search/Search";
 import { SyntheticEvent, ChangeEvent, useState } from "react";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
 
@@ -26,9 +28,19 @@ function App() {
     console.log(searchResult);
   };
 
-  const onPortfolioCreate = (e: SyntheticEvent) => {
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    console.log(e);
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) {
+      return;
+    }
+    // spread operator becuase we want to create a new array to rerender react component, dont want to update like normal
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
   };
 
   return (
@@ -38,6 +50,7 @@ function App() {
         search={search}
         handleSearchChange={handleSearchChange}
       ></Search>
+      <ListPortfolio portfolioValues={portfolioValues}></ListPortfolio>
       {/* show server error if axios api call fails (conditional rendering) */}
       {serverError && <h1>{serverError}</h1>}
       <CardList
