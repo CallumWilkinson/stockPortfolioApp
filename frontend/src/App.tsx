074aba722e8e53f603bb36006a7de7fc7b144047
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-22 14:59:49 +1100

    refactor: moved app logic to seperate concerns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,74 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { SyntheticEvent, ChangeEvent, useState } from "react";
-import { CompanySearch } from "./company";
-import { searchCompanies } from "./api";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 import NavBar from "./Components/Nav/NavBar";
-import Hero from "./Components/Hero/Hero";
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-    console.log(searchResult);
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists) {
-      return;
-    }
-    // spread operator becuase we want to create a new array to rerender react component, dont want to update like normal
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  };
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  };
-
   return (
-    <div className="App">
+    <>
       <NavBar></NavBar>
-      <Hero></Hero>
-      <Search
-        onSearchSubmit={onSearchSubmit}
-        search={search}
-        handleSearchChange={handleSearchChange}
-      ></Search>
-      <ListPortfolio
-        portfolioValues={portfolioValues}
-        onPortfolioDelete={onPortfolioDelete}
-      ></ListPortfolio>
-      {/* show server error if axios api call fails (conditional rendering) */}
-      {serverError && <h1>{serverError}</h1>}
-      <CardList
-        searchResults={searchResult}
-        onPortfolioCreate={onPortfolioCreate}
-      ></CardList>
-    </div>
+      <Outlet></Outlet>
+    </>
   );
 }
 
