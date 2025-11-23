405065ed155711ca58ee803958698f0aab00fb73
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-23 14:02:56 +1100

    feat: added getCompanyProfile api call
    - the page pulls the ticker from the url and uses that to call the api
    - gets http response with full company info
    - useEffect() wrapper allows api call to happen automatically on page load

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
+
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
+  //get the ticker from the URL, this is a react router thing
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    if (!ticker) {
+      return;
+    }
+
+    const getProfileInit = async () => {
+      try {
+        const result = await getCompanyProfile(ticker!);
+        console.log("result: ", result);
+        setCompany(result?.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getProfileInit();
+  }, [ticker]);
+
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
