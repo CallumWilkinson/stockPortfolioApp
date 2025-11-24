dd97eaceedf651099b044566c00a147cfce88a57
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-24 17:14:26 +1100

    feat: config-driven table for company dashboard

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -6,6 +6,7 @@ interface Props {}
 
 type Company = (typeof data)[0];
 
+// each config is a column
 const configs = [
   {
     label: "Year",
@@ -18,7 +19,39 @@ const configs = [
 ];
 
 const Table = (props: Props) => {
-  return <div>Table</div>;
+  const renderedRows = data.map((company) => {
+    return (
+      <tr key={company.cik}>
+        {configs.map((val: any) => {
+          return (
+            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+              {val.render(company)}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  });
+  const renderedHeaders = configs.map((config: any) => {
+    return (
+      <th
+        className="p-4 text-left text-xs font-medium text-fray-500 uppercase"
+        key={config.label}
+      >
+        {config.label}
+      </th>
+    );
+  });
+  return (
+    <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+      <table>
+        <thead className="min-w-full divide-y divide-gray-200 m-5">
+          {renderedHeaders}
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
