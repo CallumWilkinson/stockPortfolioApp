33d835c3394953193b29a004a6a1f8eaeae9dae0
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-25 19:45:15 +1100

    chore: change fahome version

--- frontend/src/Components/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Sidebar/Sidebar.tsx
@@ -18,7 +18,7 @@ const Sidebar = (props: Props) => {
                 to="company-profile"
                 className="flex md:min-w-full text-blueGrey-500 text-medium uppercase font-bold block pt--1 pb-4 no-underline"
               >
-                <FaHome />
+                <FaHome></FaHome>
                 <h6 className="ml-3">Company Profile</h6>
               </Link>
               <Link
