082d1373a9ac2b146f7f434e835be08271f8b10c
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-12-01 15:26:05 +1100

    chore: entity framework DB setup

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,19 @@
+using api.Data;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -14,5 +25,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
