01c0c9a65be46eeb8f2ab7b6393af45cd414d3de
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-12-09 18:57:35 +1100

    feat: added filtering to getAll stocks endpoint
    - repo function now takes in a QueryObject (optional) where you can search for a stock by ticker or name
    - filters results that the front end receives

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
-        Task<List<Stock>> GetAllAsync();
+        Task<List<Stock>> GetAllAsync(QueryObject query);
 
         Task<Stock?> GetByIDAsync(int id);
         Task<Stock> CreateAsync(Stock stockModel);
@@ -17,5 +18,7 @@ namespace api.Interfaces
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
         Task<Stock?> DeleteAsync(int id);
         Task<bool> StockExists(int id);
+
+
     }
 }
