2dd325e5df36aaf26ebbe61d609d21dbda841ac9
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-12-15 12:59:24 +1100

    feat: add login endpoint - login endpoint uses .NET Identity's usermanager and signinmanager to find the user and check their password - creates JWT, swagger now requires auth to use stock get all endpoint

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Account;
 using api.Dtos.Account;
 using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -17,10 +19,46 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
 
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName.ToLower());
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid Username!");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Username not found and/or password incorrect");
+            }
+
+            var newUserDto = new NewUserDto();
+            newUserDto.UserName = user.UserName;
+            newUserDto.Email = user.Email;
+            newUserDto.Token = _tokenService.CreateToken(user);
+
+            return Ok(
+                newUserDto
+            );
         }
 
         [HttpPost("register")]
--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -9,6 +9,7 @@ using api.Interfaces;
 using api.Mappers;
 using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
