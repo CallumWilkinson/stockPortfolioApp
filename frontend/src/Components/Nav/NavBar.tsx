ca1f4117d99964136626c784b1fcdaa3e6efc003
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-12-23 20:48:58 +1100

    feat(auth) add logout btn

--- frontend/src/Components/Nav/NavBar.tsx
+++ frontend/src/Components/Nav/NavBar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.png";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../Context/useAuth";
 
 interface Props {}
 
 const NavBar = (props: Props) => {
+  const { isLoggedIn, user, logout } = useAuth();
   return (
     <nav className="relative container mx-auto p-6 text-brand-forest">
       <div className="flex items-center justify-between">
@@ -21,20 +23,34 @@ const NavBar = (props: Props) => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 font-sans">
-          <Link
-            to="/login"
-            className="font-medium transition-colors hover:text-brand-leaf"
-          >
-            Login
-          </Link>
-          <Link
-            to="/register"
-            className="px-8 py-3 font-semibold rounded-lg text-brand-sand bg-brand-leaf transition-colors hover:bg-brand-mint"
-          >
-            Signup
-          </Link>
-        </div>
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 font-sans">
+            <div className="font-medium transition-colors hover:text-brand-leaf">
+              Welcome, {user?.userName}
+            </div>
+            <div
+              onClick={logout}
+              className="px-8 py-3 font-semibold rounded-lg text-brand-sand bg-brand-leaf transition-colors hover:bg-brand-mint"
+            >
+              Logout
+            </div>
+          </div>
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 font-sans">
+            <Link
+              to="/login"
+              className="font-medium transition-colors hover:text-brand-leaf"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="px-8 py-3 font-semibold rounded-lg text-brand-sand bg-brand-leaf transition-colors hover:bg-brand-mint"
+            >
+              Signup
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
