9f52909d1d38f133dd82b168893bca903ef2f9d3
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-01-02 16:16:49 +1100

    fix(router) stocks now correctly link to their pages

--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -13,7 +13,7 @@ const CardPortfolio = ({ portfolioValue, onPortfolioDelete }: Props) => {
     <>
       <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg bg-brand-mist text-brand-forest md:w-1/3">
         <Link
-          to={`/company/${portfolioValue}/company-profile`}
+          to={`/company/${portfolioValue.symbol}/company-profile`}
           className="pt-6 text-xl font-display font-semibold tracking-tight"
         >
           {portfolioValue.symbol}
